b745d2f5b5cb7cbc5e7ea7bb93f3e26f2225f91b
Michael <mgoronkov23@gmail.com>
2025-07-06 16:54:59 +0500

    Init with basic configuration

---
+++ src/test/java/org/example/finlog/FinlogApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.finlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
