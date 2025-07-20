08a1cf9dbfe2d78df23a5957b14546049aad6341
Michael <mgoronkov23@gmail.com>
2025-07-20 23:06:51 +0500

    feat: add get single transaction funcionality
    - Created getSingle() method in TransactionService
    - Extracted duplicated code into separate methods
    - Renamed getAll() method to getFiltered() in TransactionController for keeping name style consistency
    - Added tests for getSingle() method

--- src/main/java/org/example/finlog/controller/TransactionController.java
+++ src/main/java/org/example/finlog/controller/TransactionController.java
@@ -35,7 +35,7 @@ public class TransactionController {
     }
 
     @GetMapping
-    public ResponseEntity<List<Transaction>> getAll(
+    public ResponseEntity<List<Transaction>> getFiltered(
             @RequestParam(name = "category", required = false) Category category,
             @RequestParam(name = "startDate", required = false)
             @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDateTime startDate,
@@ -54,6 +54,16 @@ public class TransactionController {
         log.debug("Fetching transactions for user {}, category={}, startDate={}, endDate={}",
                 username, category, startDate, endDate);
         return ResponseEntity.ok(transactions);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Transaction> getSingle(
+            @PathVariable UUID id,
+            Principal principal
+    ) throws AccessDeniedException {
+        String username = principal.getName();
+        Transaction transaction = transactionService.getSingle(username, id);
+        return ResponseEntity.ok(transaction);
     }
 
     @PostMapping
